5c28962088876507e6cdb2bab34363f7d0a5d18d
linzeyu <linzeyu@lightgogogo.onaliyun.com>
2022-09-04 21:28:34 +0800

    add husky && lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   extends: [
-    'plugin: prettier/recommended',
+    'plugin:prettier/recommended',
     'react-app',
     'plugin:@typescript-eslint/recommended'
   ],
